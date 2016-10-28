dce1b683f138771c26ef594077a8ff5a4f214d68
Leland Clemmons <leland.clemmons@gmail.com>
2016-10-28 18:46:42 -0400

    Whitespace fixes
    This may not address all of them, but it's a start. Indentation,
    extra linebreaks, etc.

--- archive.php
+++ archive.php
@@ -19,12 +19,12 @@ get_header(); ?>
 
 <div id="page" role="main">
 	<article class="main-content">
-	<?php if ( have_posts() ) : ?>
+		<?php if ( have_posts() ) : ?>
 
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-		<?php endwhile; ?>
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+			<?php endwhile; ?>
 
 		<?php else : ?>
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
@@ -42,10 +42,8 @@ get_header(); ?>
 				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
 			</nav>
 		<?php endif; ?>
-
 	</article>
 	<?php get_sidebar(); ?>
-
 </div>
 
 <?php get_footer();
--- index.php
+++ index.php
@@ -17,12 +17,12 @@ get_header(); ?>
 
 <div id="page" role="main">
 	<article class="main-content">
-	<?php if ( have_posts() ) : ?>
+		<?php if ( have_posts() ) : ?>
 
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-		<?php endwhile; ?>
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+			<?php endwhile; ?>
 
 		<?php else : ?>
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
@@ -35,15 +35,13 @@ get_header(); ?>
 			foundationpress_pagination();
 		elseif ( is_paged() ) :
 		?>
-			<nav id="post-nav">
-				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-			</nav>
+		<nav id="post-nav">
+			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
+			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
+		</nav>
 		<?php endif; ?>
-
 	</article>
 	<?php get_sidebar(); ?>
-
 </div>
 
 <?php get_footer();
